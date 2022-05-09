fa976207e78375bbd717c443ad1babd1a805fab3
Cristian Avendaño <cavendanoc@choucairtesting.com>
2022-05-09 00:52:18 -0500

    Proyecto base terminado

---
+++ src/test/java/co/com/reto/tecnico/runners/RunnerTags.java
@@ -0,0 +1,14 @@
+package co.com.reto.tecnico.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions (features = "src/test/resources/features/demo.feature",
+        tags = "@tag1",
+        glue = " co.com.reto.tecnico",
+        snippets = SnippetType.CAMELCASE )
+
+public class RunnerTags {
+}
